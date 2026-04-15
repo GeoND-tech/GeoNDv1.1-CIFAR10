f72f950d92ff5200bf2a822fd1acaec603203ea1
GeoND.tech <dev@geond.tech>
2026-04-15 10:23:37 +0300

    Change nesterov argument type to bool

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
 parser.add_argument('--wd', default=5e-4, type=float, help='momentum')
-parser.add_argument('--nesterov', default=True, help='nesterov momentum')
+parser.add_argument('--nesterov', type=bool, default=True, help='nesterov momentum')
 parser.add_argument('--eval', type=str, help='filename to evaluate')
 parser.add_argument('--model', type=str, required=True, help='dla')
 parser.add_argument('--resume', '-r', action='store_true',
@@ -201,4 +201,3 @@ for epoch in range(start_epoch, start_epoch+200):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
-
